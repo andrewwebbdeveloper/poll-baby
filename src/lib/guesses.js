44cda96c00597a230b0255609075d3d384de6d4a
andrewwebbdeveloper <46225929+andrewwebbdeveloper@users.noreply.github.com>
2022-09-12 15:59:32 -0400

    Add date-fns, add guesses to js object

---
+++ src/lib/guesses.js
@@ -0,0 +1,18 @@
+export const guesses = {
+	H: [
+		{ name: 'Robyn', date: new Date(2022, 11, 16), time: '07:45', gender: 'boy' },
+		{ name: 'Lauren', date: new Date(2022, 11, 19), time: '05:30', gender: 'girl' },
+		{ name: 'Drew', date: new Date(2022, 11, 20), time: '08:00', gender: 'boy' },
+		{ name: 'Matt', date: new Date(2022, 11, 12), time: '14:00', gender: 'girl' },
+		{ name: 'Luke', date: new Date(2022, 11, 15), time: '23:38', gender: 'girl' },
+		{ name: 'Kaity', date: new Date(2022, 11, 17), time: '01:20', gender: 'girl' }
+	],
+	T: [
+		{ name: 'Susan', date: new Date(2022, 9, 30), time: '15:31', gender: 'girl' },
+		{ name: 'Robyn', date: new Date(2022, 9, 27), time: '03:00', gender: 'girl' },
+		{ name: 'Lauren', date: new Date(2022, 9, 27), time: '16:30', gender: 'girl' },
+		{ name: 'Drew', date: new Date(2022, 9, 30), time: '21:00', gender: 'boy' },
+		{ name: 'Alex', date: new Date(2022, 10, 2), time: '16:35', gender: 'boy' },
+		{ name: 'Matt', date: new Date(2022, 10, 6), time: '11:00', gender: 'girl' }
+	]
+};
