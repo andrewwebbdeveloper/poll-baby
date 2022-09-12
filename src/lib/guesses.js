73934a54781001e50b120074d8745a15fa42f47e
andrewwebbdeveloper <46225929+andrewwebbdeveloper@users.noreply.github.com>
2022-09-12 16:34:08 -0400

    Reformat guesses, Add vitet

--- src/lib/guesses.js
+++ src/lib/guesses.js
@@ -1,18 +1,78 @@
 export const guesses = {
 	H: [
-		{ name: 'Robyn', date: new Date(2022, 11, 16), time: '07:45', gender: 'boy' },
-		{ name: 'Lauren', date: new Date(2022, 11, 19), time: '05:30', gender: 'girl' },
-		{ name: 'Drew', date: new Date(2022, 11, 20), time: '08:00', gender: 'boy' },
-		{ name: 'Matt', date: new Date(2022, 11, 12), time: '14:00', gender: 'girl' },
-		{ name: 'Luke', date: new Date(2022, 11, 15), time: '23:38', gender: 'girl' },
-		{ name: 'Kaity', date: new Date(2022, 11, 17), time: '01:20', gender: 'girl' }
+		{
+			name: 'Robyn',
+			date: new Date(2022, 11, 16),
+			time: { hour: 7, minute: 45 },
+			gender: 'boy'
+		},
+		{
+			name: 'Lauren',
+			date: new Date(2022, 11, 19),
+			time: { hour: 5, minute: 30 },
+			gender: 'girl'
+		},
+		{
+			name: 'Drew',
+			date: new Date(2022, 11, 20),
+			time: { hour: 8, minute: 0 },
+			gender: 'boy'
+		},
+		{
+			name: 'Matt',
+			date: new Date(2022, 11, 12),
+			time: { hour: 14, minute: 0 },
+			gender: 'girl'
+		},
+		{
+			name: 'Luke',
+			date: new Date(2022, 11, 15),
+			time: { hour: 23, minute: 38 },
+			gender: 'girl'
+		},
+		{
+			name: 'Kaity',
+			date: new Date(2022, 11, 17),
+			time: { hour: 1, minute: 20 },
+			gender: 'girl'
+		}
 	],
 	T: [
-		{ name: 'Susan', date: new Date(2022, 9, 30), time: '15:31', gender: 'girl' },
-		{ name: 'Robyn', date: new Date(2022, 9, 27), time: '03:00', gender: 'girl' },
-		{ name: 'Lauren', date: new Date(2022, 9, 27), time: '16:30', gender: 'girl' },
-		{ name: 'Drew', date: new Date(2022, 9, 30), time: '21:00', gender: 'boy' },
-		{ name: 'Alex', date: new Date(2022, 10, 2), time: '16:35', gender: 'boy' },
-		{ name: 'Matt', date: new Date(2022, 10, 6), time: '11:00', gender: 'girl' }
+		{
+			name: 'Susan',
+			date: new Date(2022, 9, 30),
+			time: { hour: 15, minute: 31 },
+			gender: 'girl'
+		},
+		{
+			name: 'Robyn',
+			date: new Date(2022, 9, 27),
+			time: { hour: 3, minute: 0 },
+			gender: 'girl'
+		},
+		{
+			name: 'Lauren',
+			date: new Date(2022, 9, 27),
+			time: { hour: 16, minute: 30 },
+			gender: 'girl'
+		},
+		{
+			name: 'Drew',
+			date: new Date(2022, 9, 30),
+			time: { hour: 21, minute: 0 },
+			gender: 'boy'
+		},
+		{
+			name: 'Alex',
+			date: new Date(2022, 10, 2),
+			time: { hour: 16, minute: 35 },
+			gender: 'boy'
+		},
+		{
+			name: 'Matt',
+			date: new Date(2022, 10, 6),
+			time: { hour: 11, minute: 0 },
+			gender: 'girl'
+		}
 	]
 };
