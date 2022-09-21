8ef9927c33d34b1ffb6c4a7c12bdb0e1625172bf
andrewwebbdeveloper <46225929+andrewwebbdeveloper@users.noreply.github.com>
2022-09-21 10:58:24 -0400

    move inputs to new component

---
+++ src/lib/Inputs.svelte
@@ -0,0 +1,36 @@
+<script>
+import { onMount } from 'svelte';
+import { lightFormat, toDate, format } from 'date-fns'
+
+export let date
+export let time
+export let gender
+
+onMount(() => {
+	const today = new Date()
+	date = format(today, 'yyyy-MM-dd')
+	time = `${today.getHours().toString().padStart(2, 0)}:${today.getMinutes().toString().padStart(2, 0)}`
+	gender = 'boy'
+})
+
+</script>
+
+<div class="input-container">
+<input type="date" bind:value={date} on:input={ (e) => (date = e.target.value)}>
+<input type="time" bind:value={time}>
+<select name="" bind:value={gender}>
+	<option value="girl">girl 💃</option>
+	<option value="boy">boy 🕺🏼</option>
+</select>
+</div>
+
+<style>
+
+input, select {
+  font-size: inherit;
+}
+.input-container {
+	display: flex;
+	justify-content: center;
+}
+</style>
--- src/routes/baby-t/+page.svelte
+++ src/routes/baby-t/+page.svelte
@@ -1,19 +1,13 @@
 <script>
-import { onMount } from 'svelte';
-import { lightFormat, toDate, format } from 'date-fns'
+
 import Results from '$lib/Results.svelte'
+import Inputs from '$lib/Inputs.svelte'
 import {guesses} from '$lib/guesses.js'
+
 
 let date
 let time
 let gender
-
-onMount(() => {
-	const today = new Date()
-	date = format(today, 'yyyy-MM-dd')
-	time = `${today.getHours().toString().padStart(2, 0)}:${today.getMinutes().toString().padStart(2, 0)}`
-	gender = 'boy'
-})
 
 </script>
 
@@ -23,26 +17,25 @@ onMount(() => {
 </svelte:head>
 
 <div class="container">
-<div></div>
-<div>
-<div class="input-container">
-<input type="date" bind:value={date} on:input={ (e) => (date = e.target.value)}>
-<input type="time" bind:value={time}>
-<select name="" id="" bind:value={gender}>
-	<option value="girl">girl</option>
-	<option value="boy">boy</option>
-</select>
-</div>
-<Results guesses={guesses.T} gender={gender} time={time} date={date}/>
-</div>
+	<div></div>
+	<div>
+		<Inputs 
+		bind:date={date}
+		bind:time={time}
+		bind:gender={gender}
+		/>
+		<Results 
+		guesses={guesses.T}
+		gender={gender}
+		time={time}
+		date={date}
+		/>
+	</div>
 <div></div>
 </div>
 
 <style>
-.input-container {
-	display: flex;
-	justify-content: center;
-}
+
 	.container {
 		display: flex;
 		flex-direction: column;
