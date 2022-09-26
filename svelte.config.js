23cbfe8cce54dbabf2423894b047f2ee8260dce0
andrewwebbdeveloper <46225929+andrewwebbdeveloper@users.noreply.github.com>
2022-09-26 11:01:35 -0400

    Switch to using netlify adapter directly ( instead of auto )

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter({ edge: true })
-	}
+export default {
+  kit: {
+    adapter: adapter({ edge: true })
+  }
 };
 
-export default config;
+// import adapter from '@sveltejs/adapter-auto';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		adapter: adapter({ edge: true })
+// 	}
+// };
+
+// export default config;
